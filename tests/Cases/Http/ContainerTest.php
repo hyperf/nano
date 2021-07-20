e0b8670abfc01bba92c95482f37c8a01bdf083bf
李铭昕 <715557344@qq.com>
2021-07-20 09:53:49 +0800

    Added test cases for PHP8

--- tests/Cases/Http/ContainerTest.php
+++ tests/Cases/Http/ContainerTest.php
@@ -11,6 +11,11 @@ declare(strict_types=1);
  */
 namespace HyperfTest\Nano\Cases\Http;
 
+use Hyperf\Di\Container;
+use Hyperf\Di\Definition\DefinitionSource;
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+use Hyperf\Nano\ContainerProxy;
 use HyperfTest\Nano\HttpTestCase;
 
 /**
@@ -31,5 +36,21 @@ class ContainerTest extends HttpTestCase
         $response = $this->client()->get('/foo');
         $this->assertSame(200, $response->getStatusCode());
         $this->assertSame('bar', $response->getBody()->getContents());
+    }
+
+    public function testPsrContainer()
+    {
+        $class = new class() extends ContainerProxy {
+            public function __construct()
+            {
+                $container = new Container(new DefinitionSource([]));
+                $container->set(RequestInterface::class, null);
+                $container->set(ResponseInterface::class, null);
+                parent::__construct($container);
+            }
+        };
+
+        $class->set('id', $id = uniqid());
+        $this->assertSame($id, $class->get('id'));
     }
 }
