f8ed6a27db97d9ebd94add884714f6ddaaecfef4
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-28 13:06:07 +0800

    ⚙️chore(example) remove unused routes and middleware

--- example/swow.php
+++ example/swow.php
@@ -16,30 +16,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::createSwow();
 
 $app->get('/', function () {
-    $user = $this->request->input('user', 'nano');
-    $method = $this->request->getMethod();
-    return [
-        'message' => "hello {$user}",
-        'method' => $method,
-    ];
-});
-
-$app->addGroup('/route', function () use ($app) {
-    $app->addRoute(['GET', 'POST'], '/{id:\d+}', function ($id) {
-        return '/route/' . $id;
-    });
-    $app->put('/{name:.+}', function ($name) {
-        return '/route/' . $name;
-    });
-});
-
-$app->get('/middleware', function () {
-    return $this->request->getAttribute('key');
-});
-
-$app->addMiddleware(function ($request, $handler) {
-    $request = $request->withAttribute('key', 'value');
-    return $handler->handle($request);
+    return 'Hello World';
 });
 
 $app->run();
