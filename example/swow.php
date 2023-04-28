ce937b5389bb178974f24c706146bcd765516cf7
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-28 12:53:11 +0800

    ⚙️chore(swow) update routes and add middleware

--- example/swow.php
+++ example/swow.php
@@ -16,7 +16,30 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::createSwow();
 
 $app->get('/', function () {
-    return 'Hello World';
+    $user = $this->request->input('user', 'nano');
+    $method = $this->request->getMethod();
+    return [
+        'message' => "hello {$user}",
+        'method' => $method,
+    ];
+});
+
+$app->addGroup('/route', function () use ($app) {
+    $app->addRoute(['GET', 'POST'], '/{id:\d+}', function ($id) {
+        return '/route/' . $id;
+    });
+    $app->put('/{name:.+}', function ($name) {
+        return '/route/' . $name;
+    });
+});
+
+$app->get('/middleware', function () {
+    return $this->request->getAttribute('key');
+});
+
+$app->addMiddleware(function ($request, $handler) {
+    $request = $request->withAttribute('key', 'value');
+    return $handler->handle($request);
 });
 
 $app->run();
